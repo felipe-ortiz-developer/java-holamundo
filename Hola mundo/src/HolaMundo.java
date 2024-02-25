6198ef4765171f2bc0e2f472b4bffa0aa7ae9f70
Felipe O. <felipe.ortiz.oyarzun@gmail.com>
2024-02-25 00:35:04 -0300

    se agrega el metodo math pow para hacer potencias

--- Hola mundo/src/HolaMundo.java
+++ Hola mundo/src/HolaMundo.java
@@ -3,7 +3,11 @@ public class HolaMundo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.print("Hola Mundo");
+		System.out.println("Hola Mundo");
+		double base = 5;
+		double exponente = 3;
+		double resultado = Math.pow(base, exponente);
+		System.out.println("El resultado es "+resultado);
 	}
 
 }

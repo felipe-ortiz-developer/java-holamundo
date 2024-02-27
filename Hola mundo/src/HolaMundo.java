783fa0f2b774365bb46b0e6f01ad2521cb4a1d5b
Felipe O. <felipe.ortiz.oyarzun@gmail.com>
2024-02-27 01:52:12 -0300

    Equals

--- Hola mundo/src/HolaMundo.java
+++ Hola mundo/src/HolaMundo.java
@@ -17,6 +17,14 @@ public class HolaMundo {
 		System.out.println("Mi nombre es " + nombre + " y tiene " + numero + " letras");
 		System.out.println("La primera letra es " + nombre.charAt(0));
 		System.out.println("La ultima letra es " + nombre.charAt(numero-1));
+		
+		String frase = "Este es un curso de java para aprender a programar";
+		String resumen = frase.substring(0, frase.length());
+		System.out.println("El resumen es: " + resumen);
+		
+		String comparado = "felipe";
+		System.out.println(comparado.equals(nombre));
+		System.out.println(comparado.equalsIgnoreCase(nombre));
 	}
 
 }

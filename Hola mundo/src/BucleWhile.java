ca8ddd42f03b3821d85ad47e3a41471490a79801
Felipe O. <felipe.ortiz.oyarzun@gmail.com>
2024-03-02 17:10:05 -0300

    se agrega funcionalidad para hacer un juego de calcular el peso ideal con do while

--- Hola mundo/src/BucleWhile.java
+++ Hola mundo/src/BucleWhile.java
@@ -30,5 +30,21 @@ public class BucleWhile {
 		}
 		while(numero_random != numero);
 		System.out.println("Correcto. Lo lograste con "+intento+" intentos.");
+		
+		// Calcular el peso ideal
+		String sexo = "";
+		do {
+			sexo = JOptionPane.showInputDialog("Ingrese su sexo (h/m)");
+		} while (sexo.equalsIgnoreCase("m")==false && sexo.equalsIgnoreCase("h")==false);
+		
+		int altura = Integer.parseInt(JOptionPane.showInputDialog("Ingresa tu altura en cm"));
+		int peso_ideal = 0;
+		
+		if(sexo.equals("h")) {
+			peso_ideal = altura - 110;
+		}else if(sexo.equals("m")){
+			peso_ideal = altura - 120;
+		}
+		System.out.println("Su peso ideal es "+ peso_ideal);
 	}
 }

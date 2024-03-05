d36f562623c1698bcc981cbf2a236b84c91e81fc
Felipe O. <felipe.ortiz.oyarzun@gmail.com>
2024-03-05 00:26:04 -0300

    Recorrer array con for

--- Hola mundo/src/Arrays.java
+++ Hola mundo/src/Arrays.java
@@ -1,6 +1,7 @@
 public class Arrays {
 	public static void main(String[] args) {
 		// Arrays
+		// Formas de inicializar
 		int [] miArray2 = new int [5];
 		System.out.println(miArray2); // Da "[I@372f7a8d"
 		System.out.println(miArray2[0]); // Da 0
@@ -9,5 +10,10 @@ public class Arrays {
 		int [] miArray = {0,1,2,3,6}; 
 		System.out.println(miArray[4]); // Da 6
 		System.out.println(miArray[5]); // Da error no existe
+		
+		System.out.println("Recorriendo con for");
+		for (int i = 0; i < miArray.length; i++) {
+			System.out.println(miArray[i]);
+		}
 	}
 }

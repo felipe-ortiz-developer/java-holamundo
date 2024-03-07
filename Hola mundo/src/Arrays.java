9ee74d1508ac37c6bed70db95f51879319f96cab
Felipe O. <felipe.ortiz.oyarzun@gmail.com>
2024-03-07 17:23:36 -0300

    forEach o array mejorado

--- Hola mundo/src/Arrays.java
+++ Hola mundo/src/Arrays.java
@@ -2,18 +2,25 @@ public class Arrays {
 	public static void main(String[] args) {
 		// Arrays
 		// Formas de inicializar
-		int [] miArray2 = new int [5];
+		int [] miArray2 = new int [5]; // Queda para siempre con ese largo
 		System.out.println(miArray2); // Da "[I@372f7a8d"
 		System.out.println(miArray2[0]); // Da 0
 		System.out.println(miArray2[1]); // Da 0
 		
-		int [] miArray = {0,1,2,3,6}; 
+		int [] miArray = {0,1,2,3,6}; // Queda para siempre con ese largo
 		System.out.println(miArray[4]); // Da 6
 		// System.out.println(miArray[5]); // Da error no existe
+		miArray[4] = 11; // Puedo sobre escribir los valores de los indices
+		System.out.println(miArray[4]);
 		
 		System.out.println("Recorriendo con for");
 		for (int i = 0; i < miArray.length; i++) {
 			System.out.println(miArray[i]);
+		}
+		
+		// ForEach o "for mejorado", recorre cada elemento del array
+		for (int elemento : miArray) {
+			System.out.println("for mejorado: "+elemento);
 		}
 	}
 }

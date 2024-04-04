4215580aa353aa82f9b656665ddf2ea1d96d7877
Felipe O. <felipe.ortiz.oyarzun@gmail.com>
2024-04-04 17:13:50 -0300

    Clases, contructor, como instanciar una clase

---
+++ Hola mundo/src/poo/Carrera.java
@@ -0,0 +1,11 @@
+package poo;
+
+public class Carrera {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Coche coche1 = new Coche();
+		System.out.println(coche1.altura);
+	}
+
+}
---
+++ Hola mundo/src/poo/Coche.java
@@ -0,0 +1,11 @@
+package poo;
+
+public class Coche {
+	public int altura;
+	public int puertas;
+	
+	public Coche() {
+		altura = 12;
+		puertas = 4;
+	}
+}

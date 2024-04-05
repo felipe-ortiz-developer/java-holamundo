373c34cfd193435c7130a45d4e3c60f89ab9e65a
Felipe O. <felipe.ortiz.oyarzun@gmail.com>
2024-04-05 18:58:15 -0300

    geters and seters

--- Hola mundo/src/poo/Carrera.java
+++ Hola mundo/src/poo/Carrera.java
@@ -5,7 +5,8 @@ public class Carrera {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Coche coche1 = new Coche();
-		System.out.println(coche1.altura);
+		coche1.setAltura();
+		System.out.println(coche1.getAltura());
 	}
 
 }
--- Hola mundo/src/poo/Coche.java
+++ Hola mundo/src/poo/Coche.java
@@ -1,11 +1,19 @@
 package poo;
 
 public class Coche {
-	public int altura;
-	public int puertas;
+	private int altura;
+	private int puertas;
 	
 	public Coche() {
 		altura = 12;
 		puertas = 4;
+	}
+	
+	public void setAltura() {
+		altura =  15;
+	}
+	
+	public int getAltura() {
+		return altura;
 	}
 }

ec46d5b097a905e7d53262a1ba69afd4f8284f40
Felipe O. <felipe.ortiz.oyarzun@gmail.com>
2024-04-10 00:28:16 -0400

    Se agrega el concepto de static para compartir una variable con todas las instancias que se creen de una clase

--- Hola mundo/src/poo/Oficina.java
+++ Hola mundo/src/poo/Oficina.java
@@ -6,19 +6,26 @@ public class Oficina {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Empleado miEmpleado = new Empleado("Felipe", 1400000, 07, 04, 2024);
-		miEmpleado.aumentarSueldo();
+		Empleado miEmpleado1 = new Empleado("Felipe", 1400000, 07, 04, 2024);
+		Empleado miEmpleado2 = new Empleado("Daenerys", 4700000, 10, 04, 2024);
+		miEmpleado1.aumentarSueldo();
 //		miEmpleado.sueldo;
 		
-		System.out.println("El sueldo subio a " + miEmpleado.getSueldo());
-		System.out.println("La fecha es: "+ miEmpleado.getFechaAlta());
+		System.out.println("El sueldo subio a " + miEmpleado1.getSueldo());
+		System.out.println("La fecha es: "+ miEmpleado1.getFechaAlta());
 		
-		miEmpleado.setNombre("algo");
+//		miEmpleado.setNombre("algo"); // Da error por que nombre es constante (final)
+		System.out.println("El nombre es: "+miEmpleado1.getNombre());
+		
+		System.out.println("El id del empleado 1: "+miEmpleado1.getId());
+		System.out.println("El id del empleado 2: "+miEmpleado2.getId());
 	}
 
 }
 
 class Empleado {
+	private int id;
+	private static int idSiguiente = 1;
 	private final String nombre;
 	private int sueldo;
 	private Date fechaAlta;
@@ -28,6 +35,8 @@ class Empleado {
 			int anio,
 			int mes,
 			int dia) {
+		this.id = this.idSiguiente;
+		this.idSiguiente++;
 		this.nombre = nombre;
 		this.sueldo = sueldo;
 		
@@ -47,7 +56,15 @@ class Empleado {
 		return this.fechaAlta;
 	}
 	
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
+	public String getNombre(){
+		return this.nombre;
+	}
+	
+//	public void setNombre(String nombre) {
+//		this.nombre = nombre;
+//	}
+	
+	public int getId() {
+		return this.id;
 	}
 }

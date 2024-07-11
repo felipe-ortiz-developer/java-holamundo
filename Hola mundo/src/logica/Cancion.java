fe03a50f57f7b7756776665d690761bb93156409
Felipe O. <felipe.ortiz.oyarzun@gmail.com>
2024-07-11 00:35:02 -0400

    ejemplo de enum

---
+++ Hola mundo/src/logica/Cancion.java
@@ -0,0 +1,47 @@
+package logica;
+
+import java.util.*;
+
+
+public class Cancion {
+	private String duracion;
+	enum Estilo {
+		POP("p"),CLASICA("class"),METAL("lml"), INDIE("i");
+		private String abreviacion;
+		
+		private Estilo(String abreviacion) { // Se va a ejecutar el mismo numero de veces que el numero de valores que le das a este enum
+			//	System.out.println("Abreviacion: "+abreviacion);
+			this.setAbreviacion(abreviacion);
+		}
+		
+		public String getAbreviacion(){
+			return this.abreviacion;
+		}
+		
+		public void setAbreviacion(String abreviacion) {
+			this.abreviacion = abreviacion;
+		}
+		
+	}
+	
+	public Cancion(String duracion) {
+		this.setDuracion(duracion);
+		System.out.println("La duracion es: "+this.duracion);
+		
+		Scanner miScanner = new Scanner(System.in);
+		System.out.println("Escribe un estilo");
+		String dato = miScanner.next().toUpperCase();
+		
+		Estilo actual = Enum.valueOf(Estilo.class, dato); // enum Asignar valor
+		System.out.println("Estilo: "+actual);
+		System.out.println("Abreviatura: "+actual.getAbreviacion());
+	}
+
+	public String getDuracion() {
+		return duracion;
+	}
+
+	public void setDuracion(String duracion) {
+		this.duracion = duracion;
+	}
+}
---
+++ Hola mundo/src/logica/Reproducir.java
@@ -0,0 +1,12 @@
+package logica;
+
+public class Reproducir {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Cancion miCancion = new Cancion("1M");
+//		System.out.println("La duracion de la canción : "+miCancion.getDuracion());
+		
+	}
+
+}

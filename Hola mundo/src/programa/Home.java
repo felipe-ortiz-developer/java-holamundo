5aed3c545f4dffd972d1d09ef63dc6c6bc40b91c
Felipe O. <felipe.ortiz.oyarzun@gmail.com>
2024-07-23 22:57:18 -0400

    Eejercicio de creacion de ventana con javax.swing.JFrame

---
+++ Hola mundo/src/programa/Home.java
@@ -0,0 +1,11 @@
+package programa;
+
+import javax.swing.JFrame;
+
+public class Home extends JFrame {
+	public Home() {
+		setSize(800,500);
+		setVisible(true);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+}
---
+++ Hola mundo/src/programa/Index.java
@@ -0,0 +1,7 @@
+package programa;
+
+public class Index{
+	public static void main(String[] args) {
+		Home miHome = new Home();
+	}
+}

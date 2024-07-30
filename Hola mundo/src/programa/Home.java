4354a38dfba31ce354b699f47837f89a6e4f09d5
Felipe O. <felipe.ortiz.oyarzun@gmail.com>
2024-07-29 20:55:47 -0400

    Toolkit.getDefaultToolkit(), getScreenSize para obtener las medidas de la pantalla principal. getImage y setIconImage para cambiar el icono de la ventana

--- Hola mundo/src/programa/Home.java
+++ Hola mundo/src/programa/Home.java
@@ -1,6 +1,9 @@
 package programa;
 
+import java.awt.Dimension;
 import java.awt.Frame;
+import java.awt.Image;
+import java.awt.Toolkit;
 
 import javax.swing.JFrame;
 
@@ -8,12 +11,23 @@ public class Home extends JFrame {
 	public Home() {
 //		setLocation(500, 200);
 //		setSize(300,300);
-		setBounds(500, 200, 300, 300);
+//		setBounds(500, 200, 300, 300);
 		setVisible(true);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//		setSize
-		setResizable(false);
-		setExtendedState(Frame.MAXIMIZED_BOTH);
-		setTitle("Mi ventana");
+////		setSize
+//		setResizable(false);
+//		setExtendedState(Frame.MAXIMIZED_BOTH);
+//		setTitle("Mi ventana");
+		
+		Toolkit mipantalla = Toolkit.getDefaultToolkit();
+		Dimension midimension = mipantalla.getScreenSize();
+		int ancho = midimension.width;
+		int alto = midimension.height;
+		setSize(ancho/2, alto/2);
+		setLocation(ancho/4, alto/4);
+		Image miIcono = mipantalla.getImage("180862.PNG");
+		setIconImage(miIcono);
+		
+		
 	}
 }

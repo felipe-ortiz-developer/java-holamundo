b2313c314f5387d8b2328c60be78858a1f6f96a0
Felipe O. <felipe.ortiz.oyarzun@gmail.com>
2024-08-04 17:12:46 -0400

    para dibujar o rellenar setPaint(Color.RED); .draw o .fill. setBackground para cambiar el color de la lamina

--- Hola mundo/src/programa_v3/Home.java
+++ Hola mundo/src/programa_v3/Home.java
@@ -2,6 +2,8 @@ package programa_v3;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+
+import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.geom.Rectangle2D;
@@ -35,11 +37,16 @@ public class Home {
 		 super.paintComponent(g);
 		 Graphics2D g2 = (Graphics2D) g;
 		 Rectangle2D rectangulo = new Rectangle2D.Double(100, 100, 200, 150);
+		 g2.setPaint(Color.MAGENTA);
+		 g2.fill(rectangulo);
+		 g2.setPaint(Color.BLACK);
 		 g2.draw(rectangulo);
 		 Ellipse2D miEllipse = new Ellipse2D.Double();
 		 miEllipse.setFrame(rectangulo);
+		 g2.setPaint(Color.blue);
 		 g2.draw(miEllipse);
 		 
+		 g2.setPaint(Color.RED);
 		 g2.draw(new Line2D.Double(100, 100, 300, 250));
 		 
 		 double CentroEnX = rectangulo.getCenterX();
@@ -47,6 +54,7 @@ public class Home {
 		 double radio = 150;
 		 Ellipse2D circulo = new Ellipse2D.Double();
 		 circulo.setFrameFromCenter(CentroEnX, CentroEnY, CentroEnX+radio, CentroEnY+radio);
+		 g2.setPaint(Color.CYAN.darker());
 		 g2.draw(circulo);
 	 }
  }

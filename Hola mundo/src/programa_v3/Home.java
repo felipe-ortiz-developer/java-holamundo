dab30627cf40a203188a7768f7fc532307b76430
Felipe O. <felipe.ortiz.oyarzun@gmail.com>
2024-08-04 17:15:41 -0400

    setBackgorund(SystemColor.window);

--- Hola mundo/src/programa_v3/Home.java
+++ Hola mundo/src/programa_v3/Home.java
@@ -6,6 +6,7 @@ import javax.swing.JPanel;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.SystemColor;
 import java.awt.geom.Rectangle2D;
 import java.awt.geom.Ellipse2D;
 import java.awt.geom.Line2D;
@@ -29,7 +30,7 @@ public class Home {
 		 
 		 Lamina miLamina = new Lamina();
 		 add(miLamina);
-		 miLamina.setBackground(Color.DARK_GRAY);
+		 miLamina.setBackground(SystemColor.window);
 	 }
  }
  

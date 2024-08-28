a0fa8de968cc492d7bbc633c57ba30862b1efb7f
Felipe O. <felipe.ortiz.oyarzun@gmail.com>
2024-08-27 23:55:00 -0400

    implements WindowStateListener para usar el metodo windowStateChanged y escuchar cualquier cambio de estado

---
+++ Hola mundo/src/programa_v7/Inicio.java
@@ -0,0 +1,28 @@
+package programa_v7;
+
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowStateListener;
+
+import javax.swing.JFrame;
+
+public class Inicio {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Ventana miVentana = new Ventana(); 
+	}
+
+}
+
+class Ventana extends JFrame {
+	public Ventana() {
+		setVisible(true);
+		setBounds(700, 300, 500, 300);
+	}
+}
+
+class ListenerCambiaEstado implements WindowStateListener{
+	public void windowStateChanged(WindowEvent e){
+		System.out.println("La ventana ha cambiado de estado");
+	}
+}

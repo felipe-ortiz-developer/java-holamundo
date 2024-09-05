4e131e9295a430c6001c405c37f2ef5bcdd390e7
Felipe O. <felipe.ortiz.oyarzun@gmail.com>
2024-09-04 23:46:58 -0400

    Eventos de raton, MouseListener getX, getClickCount, mousePressed -> e.getModifiersEx() MouseMotionListener mouseDragged mouseMoved

--- Hola mundo/src/programa_v9/Inicio.java
+++ Hola mundo/src/programa_v9/Inicio.java
@@ -2,6 +2,7 @@ package programa_v9;
 
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
 
 import javax.swing.JFrame;
 
@@ -21,7 +22,25 @@ class Ventana extends JFrame {
 		
 		EventosDeRaton eventoDeRaton = new EventosDeRaton();
 		addMouseListener(eventoDeRaton);
+		EventosDeRatonMotion eventosDeRatonMotion = new EventosDeRatonMotion();
+		addMouseMotionListener(eventosDeRatonMotion);
 	}
+}
+
+class EventosDeRatonMotion implements MouseMotionListener {
+
+	@Override
+	public void mouseDragged(MouseEvent e) {
+		// TODO Auto-generated method stub
+		System.out.println("Estas arrastrando");
+	}
+
+	@Override
+	public void mouseMoved(MouseEvent e) {
+		// TODO Auto-generated method stub
+		System.out.println("Estas moviendo");
+	}
+	
 }
 
 class EventosDeRaton implements MouseListener {
@@ -30,12 +49,22 @@ class EventosDeRaton implements MouseListener {
 	public void mouseClicked(MouseEvent e) {
 		// TODO Auto-generated method stub
 		System.out.println(e);
+		System.out.println("Eje X: " + e.getX() + " / Eje Y: "+e.getY());
+		System.out.println("Count click: "+ e.getClickCount());
 	}
 
 	@Override
 	public void mousePressed(MouseEvent e) {
 		// TODO Auto-generated method stub
 		System.out.println(e);
+		System.out.println("El valor del click: "+e.getModifiersEx());
+		if ( e.getModifiersEx() == MouseEvent.BUTTON1_DOWN_MASK ) { // 1024
+			System.out.println("Has pulsado el boton izquierdo del mouse");
+		} else if (e.getModifiersEx() == MouseEvent.BUTTON2_DOWN_MASK) {
+			System.out.println("Has pulsado el boton de la rueda");
+		} else if (e.getModifiersEx() == MouseEvent.BUTTON3_DOWN_MASK) {
+			System.out.println("Has pulsado el boton derecho del mouse");
+		}
 	}
 
 	@Override
